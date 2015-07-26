0ff9127a6594eba3938dd7b12a78af868f457ef6
anlal <anlal@hotmail.com>
2015-07-26 22:40:23 +0530

    Assignment
    rprog-030

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## -----------------------------------------------------------
+## Programming Assignment 2
+## Course - rprog-030
+## Author - Aditya N. Lal (aditya.n.lal@gmail.com)
+## Github - anlal (user-id)
+## -----------------------------------------------------------
+## 
+## This program provides 2 functions -
 
-## Write a short comment describing this function
+## makeCacheMatrix(matrix):
+## 	This function accepts a matrix and wraps it within a list
+##	of functions that works with 'x' environment.
+##
+##	It is near copy of the example that has been given as part
+##	of the assignment makeVector().
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	# cinv holds cache inverse value
+	cinv <- NULL	# initial value is nil
+
+	list (
+		# define 'set' function: replaces the input matrix
+		# data with new value. It also resets the cached
+		# matrix inverse value (within 'x' env).
+		set = function(y) {
+			x <<- y
+			cinv <<- NULL
+		}
+		,
+
+		# define 'get' function: returns the matrix data
+		get = function() x
+		,
+
+		# define 'setInv' function: saves the inverse matrix
+		# value (within 'x' env)
+		setInv = function(inv) cinv <<- inv
+		,
+
+		# define 'getInv' function: retrieves cached inverse 
+		# matrix (from within 'x' env)
+		getInv = function() cinv
+	)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(cacheMatrix): 
+## This method first checks if the inverse matrix is already
+## available. Else it calculates, writes into cache and returns
+## the value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	# Retrieve the inverse matrix from cache
+	inv <- x$getInv()
+
+	if(is.null(inv)){
+		# solve for inverse over data
+		inv <- solve(x$get(),...)
+		# save the inverse within 'x' env
+		x$setInv(inv)
+	}
+	else {
+		message("from cache")
+	}
+	# Return the inverse
+	inv
 }
